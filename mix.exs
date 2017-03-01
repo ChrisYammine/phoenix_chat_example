90eb7c31ba803c94b8158f6573f22fc72f0b285b
Chris Yammine <me@chrisyammine.com>
2017-02-28 20:37:19 -0800

    Bump version to 0.0.3

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Chat.Mixfile do
 
   def project do
     [app: :chat,
-     version: "0.0.2",
+     version: "0.0.3",
      elixir: "~> 1.4",
      elixirc_paths: ["lib", "web"],
      compilers: [:phoenix] ++ Mix.compilers,
